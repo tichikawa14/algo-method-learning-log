2ad7c75877e82321be437d66aaed089fe016238a
tichikawa14 <gakusei.umeeee@gmail.com>
2022-11-16 09:41:37 +0900

    配列の全探索 4まで

--- questions/212/main.py
+++ questions/212/main.py
@@ -1,29 +1,28 @@
 """
-https://algo-method.com/tasks/210
+https://algo-method.com/tasks/212
 
 ### 問題
 $N$ 個の整数 $A_0, A_1, \dots, A_{N-1}$ が与えられます。
-$N$ 個の整数の中に整数 $V$ が何個あるかを数えるプログラムを作成してください。
+$N$ 個の整数のうち、正の整数が何個あるかを数えるプログラムを作成してください。
 ### 入力
 入力は次の形式で与えられます。
 ```IOExample
-$N$ $V$
+$N$
 $A_0 A_1 \dots A_{N-1}$
 ```
 また、入力される値は次の制約を満たします。
 - $N$ は $1$ 以上 $100$ 以下の整数
-- $V$ は $1$ 以上 $100$ 以下の整数
-- $A_i$ は $1$ 以上 $100$ 以下の整数 $(0 \leq i \leq N-1)$
+- $A_i$ は $-100$ 以上 $100$ 以下の整数 $(0 \leq i \leq N-1)$
 ### 出力
-答えを出力してください。
+$N$ 個の整数のうち $0$ より大きいものの個数を出力してください。
 """
 
-N, V = map(int, input().split())
+N = int(input())
 A = list(map(int, input().split()))
 
 count = 0
 for item in A:
-  if item == V:
+  if item > 0:
     count += 1
 
 print(count)
